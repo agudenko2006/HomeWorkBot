b33ce94b3e006a1811baf860ac268d4d2e4f76a9
Alexander Gudenko <agudenko2006@gmail.com>
2023-09-06 22:46:37 +0300

    feat: project skeleton

---
+++ README.md
@@ -0,0 +1,10 @@
+# HomeWorkBot
+
+## Getting started
+
+```bash
+export TELOXIDE_TOKEN=<Your token here>
+git clone https://github.com/agudenko2006/homeworkbot
+cd homeworkbot
+cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use color_eyre::eyre::Result;
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum Command {
+    #[command(description = "display this text")]
+    Help,
+    #[command(description = "show homework from today")]
+    Today,
+    #[command(description = "show homework for tomorrow")]
+    Tomorrow,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    pretty_env_logger::init();
+    log::info!("Starting HomeWorkBot...");
+    color_eyre::install()?;
+
+    let bot = Bot::from_env();
+
+    Command::repl(bot, answer).await;
+
+    Ok(())
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Today => {
+            bot.send_message(msg.chat.id, "Not implemented yet")
+                .await?
+        }
+        Command::Tomorrow => {
+            bot.send_message(msg.chat.id, "Not implemented yet")
+                .await?
+        }
+    };
+
+    Ok(())
+}
